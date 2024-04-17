e6d441109dba3a06419480eeeffdc8b6040923d1
Alexandra Nasirkulova <alexn10603@gmail.com>
2024-04-17 14:48:16 +0300

    additions to main classes (annotations).

--- src/main/java/com/vsu/app/entity/Client.java
+++ src/main/java/com/vsu/app/entity/Client.java
@@ -1,10 +1,19 @@
 package com.vsu.app.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@NoArgsConstructor
 @Data
 @Entity
 public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
--- src/main/java/com/vsu/app/repository/ClientRepository.java
+++ src/main/java/com/vsu/app/repository/ClientRepository.java
@@ -1,4 +1,9 @@
 package com.vsu.app.repository;
 
-public class ClientRepository {
+import com.vsu.app.entity.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
 }
