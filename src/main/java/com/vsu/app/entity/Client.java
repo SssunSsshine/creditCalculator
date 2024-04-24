bef1edfc81c1f420ce2ed54f983736ba0b30e25b
Alexandra Nasirkulova <alexn10603@gmail.com>
2024-04-24 12:59:37 +0300

    unused imports removed.

--- src/main/java/com/vsu/app/entity/Client.java
+++ src/main/java/com/vsu/app/entity/Client.java
@@ -1,6 +1,5 @@
 package com.vsu.app.entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
--- src/main/java/com/vsu/app/entity/LoanApplication.java
+++ src/main/java/com/vsu/app/entity/LoanApplication.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.math.BigDecimal;
 
